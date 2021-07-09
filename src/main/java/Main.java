d70670f43ba983ff50e735437db5adb939d2a68e
Louis Gasnault <louis.gasnault@epita.fr>
2021-07-09 22:34:36 +0200

    adds error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("coucou");
         System.out.println("c'est nous !");
+        System.err.println("toto");
     }
 }

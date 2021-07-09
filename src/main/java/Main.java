625518100ebd9f78e83a4581c018b1f78a32b027
Louis Gasnault <louis.gasnault@epita.fr>
2021-07-09 21:05:57 +0200

    removes error

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("coucou");
         System.out.println("c'est nous !");
-        System.err.println("Error");
     }
 }
